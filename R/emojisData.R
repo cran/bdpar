8b8c0b67d8847a5654525d28ae3324f236ea5515
Miguel Ferreiro-Díaz <miguel.ferreiro.diaz@gmail.com>
2022-05-18 08:30:02 +0000

    version 3.0.2

--- R/emojisData.R
+++ R/emojisData.R
@@ -21,37 +21,17 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>
 
-import email.parser
-import email.utils
-import sys
-
-def parseElement(filename,element,type):
-	parser = email.parser.Parser()
-	email_val = parser.parse(open(filename,"r"))
-	element_val=None
-
-	if element.lower()=="message":
-
-		if email_val.is_multipart():
-			for part in email_val.walk():
-				ctype = part.get_content_type()
-				cdispo = str(part.get('Content-Disposition'))
-				
-				#skip any text/plain (txt) attachments
-				if ctype == 'text/html' and 'attachment' not in cdispo:
-					element_val = part.get_payload(decode=True) #decode
-					break
-		#not multipart -i.e. plain text, no attachments, keeping fingers crossed
-		else:
-			element_val = email_val.get_payload(decode = True)
-	else:
-		element_val=email_val.get_all(element)[0]
-		
-		
-	if element_val!=None:
-		print(element_val.decode())
-	else:
-		print ("".decode())
-
-
-parseElement(sys.argv[1],sys.argv[2],sys.argv[3])
+#' Emojis codes and descriptions data.
+#'
+#' This data comes from "Unicode.org",
+#' <http://unicode.org/emoji/charts/full-emoji-list.html>. The data are codes
+#' and descriptions of Emojis.
+#'
+#' @usage data(emojisData)
+#'
+#' @format A data frame with 2623 rows and 2 variables:
+#' \describe{
+#'   \item{code}{Emoji code}
+#'   \item{description}{Emoji description.}
+#' }
+"emojisData"
